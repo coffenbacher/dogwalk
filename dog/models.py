af5902fc99014bc65999723b979efcdbf9d72b30
Charles Offenbacher <charles.offenbacher@gmail.com>
2013-04-13 16:36:40 -0700

    updating unicode rep

--- dog/models.py
+++ dog/models.py
@@ -14,8 +14,12 @@ DAYS = (
 class Dog(TimeStampedModel):
     name = models.CharField(max_length=200)
     address = models.TextField()
+    days = models.PositiveIntegerField(verbose_name = 'Days requested')
     incompatible = models.ManyToManyField('Dog')
 
+    def __unicode__(self):
+        return self.name
+        
 class RequiredWalk(TimeStampedModel):
     dog = models.ForeignKey(Dog)
     after = models.TimeField(verbose_name="Pick up after", null=True, blank=True)
@@ -26,7 +30,12 @@ class Walker(TimeStampedModel):
     name = models.CharField(max_length=200)
     address = models.TextField()
 
+    def __unicode__(self):
+        return self.name
+
 class WalkingLocation(TimeStampedModel):
     address = models.TextField()
     
+    def __unicode__(self):
+        return self.address
     
