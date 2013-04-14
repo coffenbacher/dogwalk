1661e3dab9831e9e0125c3ea69b5f795d6177cac
Charles Offenbacher <charles.offenbacher@gmail.com>
2013-04-13 18:10:29 -0700

    committing solver

--- route/migrations/0001_initial.py
+++ route/migrations/0001_initial.py
@@ -8,53 +8,82 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'NodeLocation'
-        db.create_table(u'route_nodelocation', (
+        # Adding model 'Problem'
+        db.create_table(u'route_problem', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('start', self.gf('django.db.models.fields.related.ForeignKey')(related_name='starting_at', to=orm['route.Node'])),
+            ('end', self.gf('django.db.models.fields.related.ForeignKey')(related_name='ending_at', to=orm['route.Node'])),
+        ))
+        db.send_create_signal(u'route', ['Problem'])
+
+        # Adding M2M table for field visits on 'Problem'
+        db.create_table(u'route_problem_visits', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('problem', models.ForeignKey(orm[u'route.problem'], null=False)),
+            ('node', models.ForeignKey(orm[u'route.node'], null=False))
+        ))
+        db.create_unique(u'route_problem_visits', ['problem_id', 'node_id'])
+
+        # Adding model 'Node'
+        db.create_table(u'route_node', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('address', self.gf('django.db.models.fields.TextField')()),
         ))
-        db.send_create_signal(u'route', ['NodeLocation'])
+        db.send_create_signal(u'route', ['Node'])
 
-        # Adding model 'DistanceEdge'
-        db.create_table(u'route_distanceedge', (
+        # Adding model 'Edge'
+        db.create_table(u'route_edge', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('miles', self.gf('django.db.models.fields.FloatField')()),
             ('minutes', self.gf('django.db.models.fields.FloatField')()),
         ))
-        db.send_create_signal(u'route', ['DistanceEdge'])
+        db.send_create_signal(u'route', ['Edge'])
 
-        # Adding M2M table for field locations on 'DistanceEdge'
-        db.create_table(u'route_distanceedge_locations', (
+        # Adding M2M table for field nodes on 'Edge'
+        db.create_table(u'route_edge_nodes', (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('distanceedge', models.ForeignKey(orm[u'route.distanceedge'], null=False)),
-            ('nodelocation', models.ForeignKey(orm[u'route.nodelocation'], null=False))
+            ('edge', models.ForeignKey(orm[u'route.edge'], null=False)),
+            ('node', models.ForeignKey(orm[u'route.node'], null=False))
         ))
-        db.create_unique(u'route_distanceedge_locations', ['distanceedge_id', 'nodelocation_id'])
+        db.create_unique(u'route_edge_nodes', ['edge_id', 'node_id'])
 
 
     def backwards(self, orm):
-        # Deleting model 'NodeLocation'
-        db.delete_table(u'route_nodelocation')
+        # Deleting model 'Problem'
+        db.delete_table(u'route_problem')
 
-        # Deleting model 'DistanceEdge'
-        db.delete_table(u'route_distanceedge')
+        # Removing M2M table for field visits on 'Problem'
+        db.delete_table('route_problem_visits')
 
-        # Removing M2M table for field locations on 'DistanceEdge'
-        db.delete_table('route_distanceedge_locations')
+        # Deleting model 'Node'
+        db.delete_table(u'route_node')
+
+        # Deleting model 'Edge'
+        db.delete_table(u'route_edge')
+
+        # Removing M2M table for field nodes on 'Edge'
+        db.delete_table('route_edge_nodes')
 
 
     models = {
-        u'route.distanceedge': {
-            'Meta': {'object_name': 'DistanceEdge'},
+        u'route.edge': {
+            'Meta': {'object_name': 'Edge'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'locations': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['route.NodeLocation']", 'symmetrical': 'False'}),
             'miles': ('django.db.models.fields.FloatField', [], {}),
-            'minutes': ('django.db.models.fields.FloatField', [], {})
+            'minutes': ('django.db.models.fields.FloatField', [], {}),
+            'nodes': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['route.Node']", 'symmetrical': 'False'})
         },
-        u'route.nodelocation': {
-            'Meta': {'object_name': 'NodeLocation'},
+        u'route.node': {
+            'Meta': {'object_name': 'Node'},
             'address': ('django.db.models.fields.TextField', [], {}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'route.problem': {
+            'Meta': {'object_name': 'Problem'},
+            'end': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'ending_at'", 'to': u"orm['route.Node']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'start': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'starting_at'", 'to': u"orm['route.Node']"}),
+            'visits': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['route.Node']", 'symmetrical': 'False'})
         }
     }
 
--- route/models.py
+++ route/models.py
@@ -1,10 +1,46 @@
 from django.db import models
 
 # Create your models here.
-class NodeLocation(models.Model):
-    address = models.TextField()
+class Problem(models.Model):
+    start = models.ForeignKey('Node', related_name='starting_at')
+    end = models.ForeignKey('Node', related_name='ending_at')
+    visits = models.ManyToManyField('Node')
 
-class DistanceEdge(models.Model):
-    locations = models.ManyToManyField(NodeLocation)
+    
+    #GREEDY ALGORITHM
+    def solve(self):
+        current = self.start
+        v = list(self.visits.all())
+        
+        solution = [self.start]
+
+        while v:
+            closest_d = 99999
+            
+            for i in v:
+                d = current.get_distance(i)
+                if d < closest_d:
+                    closest = i
+                    closest_d = d
+            
+            current = closest
+            v.remove(current)
+            solution.append(current)
+
+        solution.append(self.end)
+
+        return solution
+
+class Node(models.Model):
+    address = models.TextField()
+    
+    def get_distance(self, n):
+        return Edge.objects.filter(nodes__in=(self, n))[0].minutes
+    
+    def __unicode__(self):
+        return self.address
+
+class Edge(models.Model):
+    nodes = models.ManyToManyField(Node)
     miles = models.FloatField()
     minutes = models.FloatField()
