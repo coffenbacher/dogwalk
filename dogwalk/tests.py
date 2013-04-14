c5a00f5cefaefcc1f614403e143050ad2a3302ed
Charles Offenbacher <charles.offenbacher@gmail.com>
2013-04-14 15:51:40 -0700

    adding login required

--- dogwalk/tests.py
+++ dogwalk/tests.py
@@ -6,10 +6,22 @@ from django.contrib.auth.models import *
 from django.contrib.sites.models import Site
 from urllib2 import urlopen
 
-class SimpleTest(TestCase):
+class LoggedOutTest(TestCase):
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
+        User.objects.create_user(username='t@t.com', email='t@t.com', password='testests')
+
+    def test_home(self):
+        response = self.client.get('/')
+        self.failUnlessEqual(response.status_code, 302)
+
+class LoggedInTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+        User.objects.create_user(username='t@t.com', email='t@t.com', password='testests')
+        self.client.login(username='t@t.com', password='testests')
 
     def test_home(self):
         response = self.client.get('/')
--- dogwalk/views.py
+++ dogwalk/views.py
@@ -1,7 +1,9 @@
 from dog.models import *
 from schedule.models import *
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     w = Week.objects.all()
     return render_to_response('home.html', {'ws': w})
