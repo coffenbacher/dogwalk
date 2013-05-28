3ce407d2c1947cd4bb2cc8668efe1615f834d866
Charles Offenbacher <charles.offenbacher@gmail.com>
2013-05-27 20:02:24 -0700

    complete drop and create before restore

--- dogwalk/commands/load_prod_locally.sh
+++ dogwalk/commands/load_prod_locally.sh
@@ -1,4 +1,6 @@
 heroku pgbackups:capture
 curl -o latest.dump `heroku pgbackups:url`
+dropdb dogwalk
+createdb dogwalk
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d dogwalk latest.dump
 
